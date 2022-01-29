a3cc002a0007b507da411e153ed7009ed0341a32
Antoine Aflalo <Belphemur@users.noreply.github.com>
2022-01-29 14:19:11 -0500

    Fix moving average templating issue
    I don't know enough about C++ Templating to understand what is the mistake I've done in the CPP file. I prefer then to ave everything in the Header file.

--- src/AQI/MovingAverage.h
+++ src/AQI/MovingAverage.h
@@ -6,7 +6,19 @@ namespace AQI {
     template<typename T, size_t N>
     class MovingAverage {
     public:
-        MovingAverage &operator()(T sample);
+        MovingAverage &operator()(T sample) {
+            _total += sample;
+            if (_numSamples < N)
+                _samples[_numSamples++] = sample;
+            else {
+                auto index = ++_numSamples % N;
+                _numSamples = N + index;
+                T &oldest = _samples[index];
+                _total -= oldest;
+                oldest = sample;
+            }
+            return *this;
+        }
 
         operator float() const { return _total / std::min(_numSamples, N); }
 
